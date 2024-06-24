f3dd518df2ed93007d0662447fc18024b236af21
Mikhail Rubanov <akaDuality@yandex.ru>
2024-06-23 21:53:28 -0300

    Extract scene to separate class

---
+++ PizzaPhotogrammetry/Views/Detail/PizzaScene.swift
@@ -0,0 +1,165 @@
+//
+//  PizzaScene.swift
+//  PizzaPhotogrammetry
+//
+//  Created by Mikhail Rubanov on 23.06.2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SceneKit
+
+class PizzaScene: SCNScene {
+    init(url: URL, cameraMode: CameraMode) {
+        super.init()
+        
+        makeScene(url: url, cameraMode: cameraMode)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let pizzaNodeName = "Pizza"
+    
+    var pizzaNode: SCNNode! {
+        rootNode.childNode(withName: pizzaNodeName, recursively: true)
+    }
+    
+    func makeScene(url: URL, cameraMode: CameraMode) {
+        addCamera(to: self, cameraMode: cameraMode)
+        addZeroPlane(to: rootNode)
+        
+        let pizzaScene = try! SCNScene(url: url) // TODO: Remove !
+        let pizzaNode = pizzaScene.rootNode
+        pizzaNode.name = pizzaNodeName
+        
+        rootNode.addChildNode(pizzaNode)
+    }
+    
+    func transformPizzaNode(by transform: Item.Transform) {
+        let translation = SCNMatrix4Translate(SCNMatrix4Identity,
+                                              transform.translation.x,
+                                              transform.translation.y,
+                                              transform.translation.z)
+        
+        pizzaNode?.transform = translation
+        
+        pizzaNode.orientation = transform.rotation.quaternion
+    }
+    
+    private let boundingBoxNodeName = "BoundingBox"
+    func removeBox() {
+        pizzaNode
+            .childNode(withName: boundingBoxNodeName,
+                       recursively: false)?
+            .removeFromParentNode()
+    }
+    
+    func addBox(to rootNode: SCNNode, boundingBox: BoundingBox) {
+        // Clear
+        removeBox()
+        
+        // Create new
+        
+        let box = SCNBox(width:  boundingBox.width,
+                         height: boundingBox.height,
+                         length: boundingBox.length,
+                         chamferRadius: 0)
+        
+        let verticalCenter = SCNVector3(0,
+                                        boundingBox.min.y + boundingBox.height/2,
+                                        0)
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.name = boundingBoxNodeName
+        rootNode.addChildNode(boxNode)
+        
+        boxNode.position = verticalCenter
+        boxNode.geometry?.firstMaterial?.diffuse.contents = NSColor.green
+        boxNode.geometry?.firstMaterial?.transparency = 0.6
+        
+        addBoundingSpheres(to: boxNode, boundingBox: boundingBox)
+    }
+    
+    // MARK: Zero Plane
+    
+    func addZeroPlane(to roodNode: SCNNode) {
+        let box = SCNPlane(width: 1, height: 1)
+        let planeNode = SCNNode(geometry: box)
+        planeNode.name = zeroPlaneName
+        planeNode.eulerAngles.x = -.pi / 2
+        planeNode.geometry?.firstMaterial?.diffuse.contents = NSColor.purple
+        planeNode.geometry?.firstMaterial?.transparency = 0.4
+        
+        roodNode.addChildNode(planeNode)
+    }
+    
+    let zeroPlaneName = "ZeroPlane"
+    func removeZeroPlane(from rootNode: SCNNode) {
+        rootNode
+            .childNode(withName: zeroPlaneName, recursively: true)?
+            .removeFromParentNode()
+    }
+
+}
+
+func addBoundingSpheres(to box: SCNNode, boundingBox: BoundingBox) {
+    let spheres: [SCNVector3] = [
+        SCNVector3(boundingBox.width/2,
+                   boundingBox.height/2,
+                   0),
+        SCNVector3(boundingBox.max.x,
+                   0,
+                   boundingBox.max.z),
+        SCNVector3(0,
+                   boundingBox.height/2,
+                   boundingBox.length/2),
+        
+        // Center on top
+        //            SCNVector3(boundingBox.width / 2,
+        //                       boundingBox.height,
+        //                       boundingBox.length / 2)
+    ]
+    
+    spheres.forEach { coord in
+        addSphere(coord, to: box, color: .red)
+    }
+}
+
+func addSphere(_ position: SCNVector3, to rootNode: SCNNode, color: NSColor) {
+    let sphere = SCNSphere(radius: 0.005)
+    let sphereNode = SCNNode(geometry: sphere)
+    sphereNode.position = position
+    //                        coneNode.transform = SCNMatrix4Translate(.init(), 0, -0.02, 0)
+    //                        coneNode.look(at: SCNVector3(x: 0, y: 2, z: 0))
+    sphereNode.geometry?.firstMaterial?.diffuse.contents = color
+    
+    rootNode.addChildNode(sphereNode)
+}
+
+let cameraOffset: CGFloat = 0.5
+func addCamera(to scene: SCNScene, cameraMode: CameraMode) {
+    let camera = SCNCamera()
+    camera.automaticallyAdjustsZRange = true
+    camera.usesOrthographicProjection = cameraMode != .free
+    camera.orthographicScale = 0.15
+    
+    let cameraNode = SCNNode()
+    cameraNode.camera = camera
+    scene.rootNode.addChildNode(cameraNode)
+    //                    scene.pointOfView = cameraNode
+    
+    switch cameraMode {
+    case .x:
+        cameraNode.worldPosition = SCNVector3(x: cameraOffset, y: 0, z: 0)
+    case .y:
+        cameraNode.worldPosition = SCNVector3(x: 0, y: cameraOffset, z: 0)
+    case .z:
+        cameraNode.worldPosition = SCNVector3(x: 0, y: 0, z: cameraOffset)
+    case .free:
+        cameraNode.worldPosition = SCNVector3(x: cameraOffset/2, y: cameraOffset/2, z: cameraOffset/2)
+    }
+    
+    // TODO: Pass object center
+    cameraNode.look(at: SCNVector3(x: 0, y: 0, z: 0))
+}
--- PizzaPhotogrammetry/Views/Detail/PizzaSceneView.swift
+++ PizzaPhotogrammetry/Views/Detail/PizzaSceneView.swift
@@ -5,10 +5,8 @@ struct PizzaSceneView: View {
     
     @Binding var boundingBox: BoundingBox
     @Binding var transform: Item.Transform
-    @State private var scene: SCNScene
-    var pizzaNode: SCNNode! {
-        scene.rootNode.childNode(withName: "Pizza", recursively: true)
-    }
+    @State private var scene: PizzaScene
+
     
     var url: URL
     let cameraMode: CameraMode
@@ -21,9 +19,17 @@ struct PizzaSceneView: View {
         self._boundingBox = boundingBox
         self._transform = transform
         self.cameraMode = cameraMode
-        self.scene = Self.makeScene(url: url, cameraMode: cameraMode)
+        self.scene = PizzaScene(url: url, cameraMode: cameraMode)
         
         transformPizzaNode()
+    }
+    
+    func transformPizzaNode() {
+        scene.transformPizzaNode(by: $transform.wrappedValue)
+    }
+    
+    func updateBox() {
+        scene.addBox(to: scene.pizzaNode, boundingBox: boundingBox)
     }
     
     var body: some View {
@@ -38,11 +44,11 @@ struct PizzaSceneView: View {
                 ]
             )
             .onChange(of: url) { oldValue, newValue in
-                self.scene = Self.makeScene(url: url, cameraMode: cameraMode)
+                self.scene = PizzaScene(url: url, cameraMode: cameraMode)
             }.onAppear {
-                addBox(to: pizzaNode)
+                updateBox()
             }.onChange(of: boundingBox) { oldValue, newValue in
-                addBox(to: pizzaNode)
+                updateBox()
             }.onChange(of: transform) { oldValue, newValue in
                 transformPizzaNode()
             }
@@ -54,12 +60,12 @@ struct PizzaSceneView: View {
                 
                 // TODO: Potential bug: https://forums.developer.apple.com/forums/thread/704590
                 
-                removeBox()
-                removeZeroPlane(from: scene.rootNode)
+                scene.removeBox()
+                scene.removeZeroPlane(from: scene.rootNode)
                 let isSuccess = scene.write(to: url, delegate: nil)
                 
-                addBox(to: pizzaNode)
-                addZeroPlane(to: scene.rootNode)
+                updateBox()
+                scene.addZeroPlane(to: scene.rootNode)
                 
                 print("Did finish export. Success? \(isSuccess)")
             }
@@ -67,139 +73,4 @@ struct PizzaSceneView: View {
             .padding(.bottom, 16)
         }
     }
-    
-    func transformPizzaNode() {
-        let translation = SCNMatrix4Translate(SCNMatrix4Identity,
-                                              transform.translation.x,
-                                              transform.translation.y,
-                                              transform.translation.z)
-        
-        pizzaNode?.transform = translation
-        
-        pizzaNode.orientation = transform.rotation.quaternion
-    }
-    
-    static func makeScene(url: URL, cameraMode: CameraMode) -> SCNScene {
-        let rootScene = SCNScene()
-        
-        addCamera(to: rootScene, cameraMode: cameraMode)
-        addZeroPlane(to: rootScene.rootNode)
-        
-        let pizzaScene = try! SCNScene(url: url) // TODO: Remove !
-        let pizzaNode = pizzaScene.rootNode
-        pizzaNode.name = "Pizza"
-        
-        rootScene.rootNode.addChildNode(pizzaNode)
-        
-        return rootScene
-    }
-    
-    private func removeBox() {
-        pizzaNode.childNode(withName: "BoundingBox", recursively: false)?.removeFromParentNode()
-    }
-    
-    func addBox(to rootNode: SCNNode) {
-        // Clear
-        removeBox()
-        
-        // Create new
-        
-        let box = SCNBox(width:  boundingBox.width,
-                         height: boundingBox.height,
-                         length: boundingBox.length,
-                         chamferRadius: 0)
-
-        let verticalCenter = SCNVector3(0,
-                                        boundingBox.min.y + boundingBox.height/2,
-                                        0)
-        
-        let boxNode = SCNNode(geometry: box)
-        boxNode.name = "BoundingBox"
-        rootNode.addChildNode(boxNode)
-        
-        boxNode.position = verticalCenter
-        boxNode.geometry?.firstMaterial?.diffuse.contents = NSColor.green
-        boxNode.geometry?.firstMaterial?.transparency = 0.6
-        
-        addBoundingSpheres(to: boxNode, boundingBox: boundingBox)
-    }
-}
-
-func addBoundingSpheres(to box: SCNNode, boundingBox: BoundingBox) {
-    let spheres: [SCNVector3] = [
-        SCNVector3(boundingBox.width/2,
-                   boundingBox.height/2,
-                   0),
-        SCNVector3(boundingBox.max.x,
-                   0,
-                   boundingBox.max.z),
-        SCNVector3(0,
-                   boundingBox.height/2,
-                   boundingBox.length/2),
-        
-        // Center on top
-        //            SCNVector3(boundingBox.width / 2,
-        //                       boundingBox.height,
-        //                       boundingBox.length / 2)
-    ]
-    
-    spheres.forEach { coord in
-        addSphere(coord, to: box, color: .red)
-    }
-}
-
-func addSphere(_ position: SCNVector3, to rootNode: SCNNode, color: NSColor) {
-    let sphere = SCNSphere(radius: 0.005)
-    let sphereNode = SCNNode(geometry: sphere)
-    sphereNode.position = position
-    //                        coneNode.transform = SCNMatrix4Translate(.init(), 0, -0.02, 0)
-    //                        coneNode.look(at: SCNVector3(x: 0, y: 2, z: 0))
-    sphereNode.geometry?.firstMaterial?.diffuse.contents = color
-    
-    rootNode.addChildNode(sphereNode)
-}
-
-let cameraOffset: CGFloat = 0.5
-func addCamera(to scene: SCNScene, cameraMode: CameraMode) {
-    let camera = SCNCamera()
-    camera.automaticallyAdjustsZRange = true
-    camera.usesOrthographicProjection = cameraMode != .free
-    camera.orthographicScale = 0.15
-    
-    let cameraNode = SCNNode()
-    cameraNode.camera = camera
-    scene.rootNode.addChildNode(cameraNode)
-    //                    scene.pointOfView = cameraNode
-    
-    switch cameraMode {
-    case .x:
-        cameraNode.worldPosition = SCNVector3(x: cameraOffset, y: 0, z: 0)
-    case .y:
-        cameraNode.worldPosition = SCNVector3(x: 0, y: cameraOffset, z: 0)
-    case .z:
-        cameraNode.worldPosition = SCNVector3(x: 0, y: 0, z: cameraOffset)
-    case .free:
-        cameraNode.worldPosition = SCNVector3(x: cameraOffset/2, y: cameraOffset/2, z: cameraOffset/2)
-    }
-    
-    // TODO: Pass object center
-    cameraNode.look(at: SCNVector3(x: 0, y: 0, z: 0))
-}
-
-func addZeroPlane(to roodNode: SCNNode) {
-    let box = SCNPlane(width: 1, height: 1)
-    let planeNode = SCNNode(geometry: box)
-    planeNode.name = zeroPlaneName
-    planeNode.eulerAngles.x = -.pi / 2
-    planeNode.geometry?.firstMaterial?.diffuse.contents = NSColor.purple
-    planeNode.geometry?.firstMaterial?.transparency = 0.4
-    
-    roodNode.addChildNode(planeNode)
-}
-
-let zeroPlaneName = "ZeroPlane"
-func removeZeroPlane(from rootNode: SCNNode) {
-    rootNode
-        .childNode(withName: zeroPlaneName, recursively: true)?
-        .removeFromParentNode()
 }
